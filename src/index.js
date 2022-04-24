d1efe7381552b1a0f02455a37083b024487e1423
Ramshad <cpramshad@gmail.com>
2022-04-24 20:41:45 +0400

    Add heading component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@
 // addImage();
 
 import helloWorldButton from './components/hello-world-button/hello-world-button';
+import heading from './components/heading/heading';
+
+const headingObj = new heading();
+headingObj.render();
 
 const helloWorldButtonObj = new helloWorldButton();
 helloWorldButtonObj.render();
