99d97debeb0f6c8e9bf18f814a9f9fc712d0237a
Ramshad <cpramshad@gmail.com>
2022-04-24 13:12:21 +0400

    Add publicPath and set 3 kb limit for asset/inline and asset/resource module type

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,21 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
     },
     mode: 'none',
     module: {
         rules: [
             {
                 test: /\.(png|jpg|jpeg)$/,
-                type: 'asset/resource'
+                type: 'asset/resource',
+                parser: {
+                    // less than 3 kb asset/inline else asset/resource
+                    defaultUrlCondition: {
+                        maxSize: 3 * 1024 // 3 kb
+                    }
+                }
             }
         ]
     }
