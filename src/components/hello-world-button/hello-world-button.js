93ab53a155ff7c0a5bdc64428049295bfcfc024c
Ramshad <cpramshad@gmail.com>
2022-04-24 19:37:45 +0400

    Add babel loader in wepack config

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,10 +1,11 @@
 // import './hello-world-button.css';
 import './hello-world-button2.scss';
 class helloWorldButton {
+    buttonCssClass = 'hello-world-button';
     render() {
         const button = document.createElement('button');
         button.innerHTML = 'Hello world!';
-        button.classList.add('hello-world-button');
+        button.classList.add(this.buttonCssClass);
         const body = document.querySelector('body');
         button.onclick = function() {
             const p = document.createElement('p');
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,16 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader', 'sass-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_mocules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env']
+                    }
+                }
             }
         ]
     }
