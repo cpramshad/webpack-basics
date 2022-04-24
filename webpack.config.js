67c3cc4fd9c16512ca41c2dc551ba1c6ecaff014
Ramshad <cpramshad@gmail.com>
2022-04-24 20:22:43 +0400

    Add TerserPlugin for bundle minification?

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -47,5 +48,8 @@ module.exports = {
                 }
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new TerserPlugin()
+    ]
+} 
